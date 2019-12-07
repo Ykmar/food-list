cdf7f92dc0980254f400f2ec927e4d1a3e8318a7
ma.collet <marc.collet2@hotmail.fr>
2019-12-07 16:53:19 +0100

    creation des premieres vues

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/recipes') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -34,7 +34,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="">{{ __('Recettes') }}</a>
+                            <a class="nav-link" href="{{ route('recipes.index') }}">{{ __('Recettes') }}</a>
                         </li>
                     </ul>
 
---
+++ resources/views/recipes/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    Recettes
+                </div>
+                <div class="card-body">
+                    <a href="{{ route('recipes.create') }}" class="btn btn-primary btn-block mb-2">Créer une recette</a>
+                    @foreach($recipes as $recipe)
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <div class="row align-items-center">
+                                    <div class="col-md-4">
+                                        <h5 class="card-title">
+                                            {{ $recipe->name }}
+                                            (<a href="{{ route('recipes.show', $recipe) }}">voir</a>)
+                                            (<a href="{{ route('recipes.edit', $recipe) }}">éditer</a>)
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
